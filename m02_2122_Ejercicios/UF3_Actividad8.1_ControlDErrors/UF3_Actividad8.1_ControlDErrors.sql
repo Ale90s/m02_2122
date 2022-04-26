3aaa0cb0daa17e55462426369bb13eab0dcf0da0
Ale90s <82205418+Ale90s@users.noreply.github.com>
2022-04-26 19:34:52 +0200

    Update UF3_Actividad8.1_ControlDErrors.sql

--- m02_2122_Ejercicios/UF3_Actividad8.1_ControlDErrors/UF3_Actividad8.1_ControlDErrors.sql
+++ m02_2122_Ejercicios/UF3_Actividad8.1_ControlDErrors/UF3_Actividad8.1_ControlDErrors.sql
@@ -1,7 +1,36 @@
 -- Tasca 1. Crea un procediment que donat un nombre que representa l’any, genera una taula com la de comandes, 
 -- però amb el nom comandes_ANY (on ANY és el nombre que ens han passat com a paràmetre) si aquesta taula no existeix.
 
+DELIMITER //
+CREATE OR REPLACE PROCEDURE nomByAny(IN año INT)
+BEGIN
 
+DECLARE tasca1 
+	HANDLER FOR 1146
+    CREATE TABLE CONCAT(comandes_, año) (
+		numero SMALLINT AUTO_INCREMENT,
+		data_hora TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		domicili_local ENUM('D', 'L') NOT NULL,
+		client_id SMALLINT NOT NULL,
+		empleat_id SMALLINT(3) NOT NULL,
+		PRIMARY KEY (numero),
+    ) ENGINE = INNODB;
+
+SELECT 1 
+	FROM CONCAT(comandes_, año);
+
+END //
+DELIMITER ;
+
+SET @var = "comandes_";
+
+SET @var = @var + "a";
+SELECT @var;
+
+SELECT 1 
+	FROM CONCAT(comandes_, año);
+
+SELECT * FROM comanda;
 
 -- Tasca 2. Crea un procediment que copiï les comandes en una taula anomenada comandes_ANY 
 -- ( on ANY corresponen l’any de la data d’un paràmetre d’entrada), només copiarem totes aquelles comandes que corresponen 
