7687555043597dbda97fcc69925d2026a788da01
Ale90s <82205418+Ale90s@users.noreply.github.com>
2022-04-26 19:08:07 +0200

    Create UF3_Actividad8.1_ControlDErrors.sql

---
+++ m02_2122_Ejercicios/UF3_Actividad8.1_ControlDErrors/UF3_Actividad8.1_ControlDErrors.sql
@@ -0,0 +1,17 @@
+-- Tasca 1. Crea un procediment que donat un nombre que representa l’any, genera una taula com la de comandes, 
+-- però amb el nom comandes_ANY (on ANY és el nombre que ens han passat com a paràmetre) si aquesta taula no existeix.
+
+
+
+-- Tasca 2. Crea un procediment que copiï les comandes en una taula anomenada comandes_ANY 
+-- ( on ANY corresponen l’any de la data d’un paràmetre d’entrada), només copiarem totes aquelles comandes que corresponen 
+-- a aquest ANY. Si la taula no existeix, fes que es generi a partir del procediment de la Tasca 1. Alhora, sí aquella data 
+-- és de l’any actual, avisa a l’usuari que l’any està en curs i no es pot copiar la informació amb un paràmetre de sortida.
+
+
+
+-- Tasca 3. Crea un procediment anomenat getTotalComandesCopiaByAny que donat un nombre que correspon a un any, 
+-- ens mostri el nombre de comandes que s'han fet en aquest any. Per tant, per comptar aquestes comandes s’ha d’usar la 
+-- taula corresponent a comandes_ANY (gestionada anteriorment). Tingues en compte que si no s'ha realitzat cap còpia de 
+-- comandes ha de mostrar 0.
+
