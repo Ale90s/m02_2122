24905c7927dec3998f455c34fe4040549806a249
Ale90s <82205418+Ale90s@users.noreply.github.com>
2022-04-27 10:50:38 +0200

    Update UF3_Actividad.6.3_ProgramacioAmbWhile.sql

--- m02_2122_Ejercicios/UF3_Actividad6.3_ProgramacioAmbWhile/UF3_Actividad.6.3_ProgramacioAmbWhile.sql
+++ m02_2122_Ejercicios/UF3_Actividad6.3_ProgramacioAmbWhile/UF3_Actividad.6.3_ProgramacioAmbWhile.sql
@@ -1,19 +1,19 @@
--- Tasca 1. Realitza la transformació en un nou procediment de la Tasca 1 de l’Activitat 6.3 Programació amb WHILE, 
--- pero ara usant REPEAT.
+-- Tasca 1. Crea un procediment que donat dues dates, esborri totes les reunions compreses entre aquells dies.
+--  Si no hi ha reunions a esborrar o les dates no s'informen, retornar un avís d'error.
 
 
 
--- Tasca 2. Realitza la transformació en un nou procediment de la Tasca 2 de l’Activitat 6.3 Programació amb WHILE, 
--- pero ara usant REPEAT.
+-- Tasca 2. Crea un procediment que generi reunions, de manera que inserirà un esdeveniment repetit cada cert temps. 
+-- Els paràmetres que li donarem seran els següents:
 /*
-Data d'inici de les insercions,
-Data final de les insercions,
-Hora de l'esdeveniment,
-Esdeveniment
-Identificador del Tipus de reunió, recordeu que les reunions de:
-Caps assisteixen els empleats que són caps,
-a les reunions General assisteixen tots els empleats
-i les reunions de Personal només aquells que no són caps
-Freqüència: D (diària), W (setmanal), M (mensual) i Y (anual).
+	* Data d'inici de les insercions,
+	* Data final de les insercions,
+	* Hora de l'esdeveniment,
+	* Esdeveniment
+	* Identificador del Tipus de reunió, recordeu que les reunions de:
+			*Caps assisteixen els empleats que són caps,
+			*A les reunions General assisteixen tots els empleats
+			*O les reunions de Personal només aquells que no són caps
+	* Freqüència: D (diària), W (setmanal), M (mensual) i Y (anual).
 Genera diverses reunions per provar algunes de les combinacions possibles.
 */
