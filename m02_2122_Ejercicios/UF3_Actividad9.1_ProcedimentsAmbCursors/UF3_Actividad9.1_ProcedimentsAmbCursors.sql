0a1383bc08e7894211ecc33b0027a96c5df01b37
Ale90s <82205418+Ale90s@users.noreply.github.com>
2022-05-02 14:16:46 +0200

    Create UF3_Actividad9.1_ProcedimentsAmbCursors.sql

---
+++ m02_2122_Ejercicios/UF3_Actividad9.1_ProcedimentsAmbCursors/UF3_Actividad9.1_ProcedimentsAmbCursors.sql
@@ -0,0 +1,30 @@
+-- Tasca1. 
+/*
+Afegeix una columna nova la taula clients que s’anomenti num_comandes. 
+Aquest nou camp contindrà la quantitat de comandes diferents que ha realitzat aquell client.
+
+Crea un procediment emmagatzemat que, sense rebre cap paràmetre, 
+ompli la columna num_comandes amb el nombre de comandes que ha fet cada client.
+*/
+
+
+
+-- Tasca 2. 
+/*
+Afegeix una columna nova a la taula empleats que s’anomeni total_facturacio. 
+Contindrà la facturació total que ha fet aquell empleat, excloent-hi l'IVA. 
+Per a aquesta activitat, suposarem que tots els preus dels productes són amb IVA inclòs.
+Crea un procediment emmagatzemat que, rebent per paràmetre un enter que representa el % d'IVA (per exemple, 10), 
+ompli la columna de quantitat de facturació total de l'empleat sense comptar l'IVA.
+*/
+
+
+
+-- Tasca 3.
+/*
+Crea un procediment anomenat duplicaProductesByTipus. El procediment rebrà per paràmetre el tipus de
+productes a duplicar (‘B’ per a les begudes, ‘D’ pels postres i ‘P’ per a les pizzes).
+El procediment duplicarà a la taula o taules corresponents tots els productes d'aquell tipus, 
+afegint al final del nom del producte s'hi afegirà el text "(còpia)". Per exemple, si estem duplicant 
+les begudes i tenim una 'Ampolla Coca-Cola' es crearà un nou producte anomenat 'Ampolla Coca-Cola (còpia)’.
+*/
