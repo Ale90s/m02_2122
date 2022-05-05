140a7c748b9654e11cac28a423b55bd852937174
Ale90s <82205418+Ale90s@users.noreply.github.com>
2022-05-05 19:53:17 +0200

    Ejercicio 1

--- m02_2122_Ejercicios/UF3_Actividad10.1_FuncionsSimplesDefinidesPerLUsuari/UF3_Actividad10.1_FuncionsSimplesDefinidesPerLUsuari.sql
+++ m02_2122_Ejercicios/UF3_Actividad10.1_FuncionsSimplesDefinidesPerLUsuari/UF3_Actividad10.1_FuncionsSimplesDefinidesPerLUsuari.sql
@@ -1,3 +1,5 @@
+USE uf2_p2_pizzeria;
+
 -- Tasca 1. 
 /*
 Volem crear una funció anomenada getDescripcioQualificacio, on ha partir d’un parametre tipus string variable,
@@ -5,7 +7,24 @@ sí s’envia amb valor 'apte' retorni una 'A', en cas de 'no apte' retorni 'NA'
 opcions retornarà 'NS/NC'.
 */
 
+DELIMITER //
+CREATE OR REPLACE FUNCTION getDescripcioQualificacio (opcio VARCHAR(15))
+RETURNS VARCHAR(5)
+	IF(opcio = "apte") THEN
+		RETURN "A";
+	ELSEIF(opcio = "no apte") THEN
+		RETURN "NA";
+	ELSE
+		RETURN "NS/NC";
+	END IF;
+END //
+DELIMITER ;
 
+SELECT getDescripcioQualificacio("Opcio no valida");
+SELECT getDescripcioQualificacio("apte");
+SELECT getDescripcioQualificacio("no apte");
+
+DROP FUNCTION getDescripcioQualificacio;
 
 -- Tasca 2. Crea una funció anomenada getApteByNota que retorni el valor 'apte' 
 -- si escrivim una nota que és superior a 5, i 'no apte' en cas contrari.
@@ -32,4 +51,3 @@ En cas que no sigui cap d'aquest que escrigui un guió '-'.
 
 
 -- Tasca 6. Crea una funció anomenada getValorAbsolut de manera que sumi dos valors enters en valor absolut.
-
