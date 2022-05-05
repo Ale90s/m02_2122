85152f45903a76992c7c12ec625c7cb020f2edd3
Ale90s <82205418+Ale90s@users.noreply.github.com>
2022-05-05 21:04:08 +0200

    Create UF3_Actividad10.2_FuncionsAgregadesDefinidesPerLUsuari.sql

---
+++ m02_2122_Ejercicios/UF3_Actividad10.2_FuncionsAgregadesDefinidesPerLUsuari/UF3_Actividad10.2_FuncionsAgregadesDefinidesPerLUsuari.sql
@@ -0,0 +1,12 @@
+USE uf2_p2_pizzeria;
+
+-- Tasca 1. Volem crear una funció anomenada getSumaEnters qeu realitzi les funcions de SUM per valors enters.
+
+
+
+-- Tasca 2. Volem crear una funció anomenada getRestaEnters que realitzi les funcions de SUM pero restan els valors enters.
+
+
+
+-- Tasca 3. Volem crear una funció anomenada getMinEnters que realitzi les funcions de MIN per valors enters.
+
