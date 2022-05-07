bb9f01251ee9e0dccc13e0d0aca89cbc12f36af5
Ale90s <82205418+Ale90s@users.noreply.github.com>
2022-05-07 18:05:04 +0200

    Create UF3_Actividad10.3_FuncionsBDDefinidesPerLUsuari.sql

---
+++ m02_2122_Ejercicios/UF3_Actividad10.3_FuncionsBDDefinidesPerLUsuari/UF3_Actividad10.3_FuncionsBDDefinidesPerLUsuari.sql
@@ -0,0 +1,16 @@
+USE uf2_p2_pizzeria;
+
+-- Tasca 1. Crea una funció que realitzi la funcionalitat de la Tasca 1 de M02 UF2 Activitat 9.1 - Consultes niades.
+
+
+
+-- Tasca 2. Crea una funció que realitzi la funcionalitat de la Tasca 10 de M02 UF2 Activitat 9.1 - Consultes niades.
+
+
+
+-- Tasca 3. Crea una funció que realitzi la funcionalitat de la Tasca 11 de M02 UF2 Activitat 9.1 - Consultes niades.
+
+
+
+-- Tasca 4. Mostra a partir d'una consulta tipus SELECT a la taula productes, si un producte es pizza, beguda o postre.
+
