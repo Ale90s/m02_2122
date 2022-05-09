25b9f1501aa56e49ef924d7e0bc0d0dd4fed6276
Ale90s <82205418+Ale90s@users.noreply.github.com>
2022-05-09 12:26:16 +0200

    Create UF3_Actividad11.1_ProvesAmbDisparadors.sql

---
+++ m02_2122_Ejercicios/UF3_Actividad11.1_ProvesAmbDisparadors/UF3_Actividad11.1_ProvesAmbDisparadors.sql
@@ -0,0 +1,22 @@
+-- Tasca 1. Crea un disparador que quan fem una inserció a la taula prova, 
+-- deixi només l'últim registre introduït a la taula suma.
+
+
+
+-- Tasca 2. Crea els disparadors que compti els registres de la taula prova, 
+-- tant si inserim com si esborrem,  i crea un registre amb aquest nombre a la taula suma.
+
+
+
+-- Tasca 3. Crea un disparador que cada vegada que inserim un registre a la taula prova, 
+-- introdueixi el valor a la taula suma_text amb el signe + davant. 
+-- Així si a la taula prova tenim els valors 1, 2 i 3, a la taula suma_text hi haurà un 
+-- camp de text amb valor 1+2+3.
+
+
+
+-- Tasca 4. Creem una taula anomenada prova_log que té tres camps (hora i data, usuari, acció). 
+-- Crea un disparador que faci un registre de tots els canvis que fem a la taula prova, 
+-- tant sigui inserir, esborrar o modificar. Així si fem gestions a la taula prova escriuria a 
+-- prova_log, per exemple:
+
