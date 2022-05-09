89d8b07373233630673f0e64e83c79ee96bab73f
Ale90s <82205418+Ale90s@users.noreply.github.com>
2022-05-09 14:21:07 +0200

    Update UF3_Actividad11.1_ProvesAmbDisparadors.sql

--- m02_2122_Ejercicios/UF3_Actividad11.1_ProvesAmbDisparadors/UF3_Actividad11.1_ProvesAmbDisparadors.sql
+++ m02_2122_Ejercicios/UF3_Actividad11.1_ProvesAmbDisparadors/UF3_Actividad11.1_ProvesAmbDisparadors.sql
@@ -1,12 +1,38 @@
+
+DROP DATABASE IF EXISTS proves_disparadors;
+CREATE DATABASE IF NOT EXISTS proves_disparadors;
+USE proves_disparadors;
+
+CREATE TABLE prova (codi INT);
+CREATE TABLE suma (suma INT);
+CREATE TABLE suma_text (suma VARCHAR(200));
+
 -- Tasca 1. Crea un disparador que quan fem una inserció a la taula prova, 
 -- deixi només l'últim registre introduït a la taula suma.
 
+INSERT INTO suma (suma) VALUES (LAST_INSERT_ID(500));
+INSERT INTO suma (suma) VALUES (LAST_INSERT_ID(96));
+INSERT INTO suma (suma) VALUES (LAST_INSERT_ID(31));
+INSERT INTO suma (suma) VALUES (LAST_INSERT_ID(123));
 
+DELIMITER //
+CREATE OR REPLACE TRIGGER triggProva AFTER INSERT ON prova FOR EACH ROW
+BEGIN
+	DELETE FROM suma;
+    INSERT INTO suma VALUES (NEW.codi);
+END //
+DELIMITER ;
+
+INSERT INTO prova (codi) VALUES (10);
+
+SELECT * 
+	FROM suma;
 
 -- Tasca 2. Crea els disparadors que compti els registres de la taula prova, 
 -- tant si inserim com si esborrem,  i crea un registre amb aquest nombre a la taula suma.
 
-
+SELECT COUNT(*)
+	FROM prova;
 
 -- Tasca 3. Crea un disparador que cada vegada que inserim un registre a la taula prova, 
 -- introdueixi el valor a la taula suma_text amb el signe + davant. 
@@ -19,4 +45,3 @@
 -- Crea un disparador que faci un registre de tots els canvis que fem a la taula prova, 
 -- tant sigui inserir, esborrar o modificar. Així si fem gestions a la taula prova escriuria a 
 -- prova_log, per exemple:
-
