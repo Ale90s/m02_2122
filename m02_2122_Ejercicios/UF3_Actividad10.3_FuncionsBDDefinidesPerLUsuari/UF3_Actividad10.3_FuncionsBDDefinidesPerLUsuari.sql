431ab7c2392c531277daee221e33ff45aeed15c5
Ale90s <82205418+Ale90s@users.noreply.github.com>
2022-05-09 13:27:05 +0200

    Ejercicios acabados.

--- m02_2122_Ejercicios/UF3_Actividad10.3_FuncionsBDDefinidesPerLUsuari/UF3_Actividad10.3_FuncionsBDDefinidesPerLUsuari.sql
+++ m02_2122_Ejercicios/UF3_Actividad10.3_FuncionsBDDefinidesPerLUsuari/UF3_Actividad10.3_FuncionsBDDefinidesPerLUsuari.sql
@@ -7,7 +7,9 @@ DELIMITER //
 CREATE OR REPLACE FUNCTION muestraIngredientes()
 RETURNS INT
 BEGIN
-	RETURN (SELECT MAX(numero) FROM comanda);
+
+RETURN (SELECT MAX(numero) FROM comanda);
+
 END //
 DELIMITER ;
 
@@ -40,19 +42,58 @@ WHERE TIME(co.data_hora) = horaMasTarde();
 
 
 -- Tasca 3. Crea una funció que realitzi la funcionalitat de la Tasca 11 de M02 UF2 Activitat 9.1 - Consultes niades.
+-- Mostra els ingredients de les pizzes que s'han demanat més vegades en una comanda.
 
-/*
+DELIMITER //
+CREATE OR REPLACE FUNCTION ingPizzesMasPedidas()
+RETURNS INT
+BEGIN
+
+	RETURN (SELECT MAX(copr.quantitat)
+						FROM comanda_producte copr
+							INNER JOIN producte pr ON copr.id_producte = pr.id_producte
+							INNER JOIN pizza piz ON pr.id_producte = piz.id_producte);
+
+END //
+DELIMITER ;
+                            
 SELECT DISTINCT ing.nom
 FROM ingredient ing
 	INNER JOIN pizza_ingredient pizing  ON ing.id_ingredient = pizing.id_ingredient
     INNER JOIN pizza piz ON pizing.id_producte = piz.id_producte
     INNER JOIN producte pr ON piz.id_producte = pr.id_producte
 	INNER JOIN comanda_producte copr ON pr.id_producte = copr.id_producte
-WHERE copr.quantitat = (SELECT MAX(quantitat)
-						FROM comanda_producte copr
-							INNER JOIN producte pr ON copr.id_producte = pr.id_producte
-							INNER JOIN pizza piz ON pr.id_producte = piz.id_producte);
-*/
+WHERE copr.quantitat = (ingPizzesMasPedidas());
+
 
 -- Tasca 4. Mostra a partir d'una consulta tipus SELECT a la taula productes, si un producte es pizza, beguda o postre.
 
+DELIMITER //
+CREATE OR REPLACE FUNCTION tipoProducto(ident_producto INT)
+RETURNS VARCHAR(50)
+BEGIN
+
+IF(SELECT 1 FROM pizza WHERE id_producte = ident_producto) THEN
+
+RETURN "Es una pizza";
+
+ELSEIF(SELECT 1 FROM beguda WHERE id_producte = ident_producto) THEN
+
+RETURN "Es una bebida";
+
+ELSEIF(SELECT 1 FROM postre WHERE id_producte = ident_producto) THEN
+
+RETURN "Es un postre";
+
+ELSE 
+
+RETURN "No se ha encontrado el producto";
+
+END IF;
+END //
+DELIMITER ;
+
+SELECT tipoProducto(4);
+SELECT tipoProducto(10);
+SELECT tipoProducto(100);
+SELECT tipoProducto(13);
